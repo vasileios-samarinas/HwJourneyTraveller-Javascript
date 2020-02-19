a2130b299963ccada06d7d6903a1b33cfa348c44
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-19 20:42:43 +0000

    Initial Commit

---
+++ models/journey.js
@@ -0,0 +1,13 @@
+const Journey = function(startLocation, endLocation, transport, distance) {
+  this.startLocation = startLocation;
+  this.endLocation = endLocation;
+  this.transport = transport;
+  this.distance = distance;
+};
+
+module.exports = Journey;
+
+
+Journey.prototype.startLocation = function () {
+
+};
